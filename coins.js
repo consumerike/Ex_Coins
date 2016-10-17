2a4142b9c1eb5f8f6b12678dc2e8f1f985392243
Ikechi <consumerike@gmail.com>
2016-10-17 10:16:32 -0500

    added readme, coins.js, index.html

---
+++ coins.js
@@ -0,0 +1,47 @@
+
+function coins(amount) {
+	// console.log(this.quarters = amount / (.25));
+	// console.log(this.dimes = amount / (.10));
+	// console.log(this.nickels = amount / (.05));
+	// console.log(this.pennies = amount / (.01));
+		var quarters = .25;
+		var dimes = .10;
+		var nickels = .05;
+		var pennies = .01;
+		var exactAmount = (amount) / (quarters);
+		if (exactAmount > 1) {
+	    	var numQuarters = Math.floor((amount) / (quarters));	
+	    	console.log("Quarters: " + numQuarters);
+	    	amount -= (numQuarters * quarters);
+		}
+	    else if ((amount) / (dimes) > 1) {
+			var numDimes = Math.floor((amount) / (dimes));
+			console.log("Dimes: " +numDimes);
+			amount -= (numDimes * dimes); 
+		}
+
+	   else if ((amount) / (nickels) > 1) {
+			var numNickels = Math.floor((amount) / (nickels));
+			console.log("Nickels: " +numNickels);
+			amount -= (numNickels * nickels); 
+		}
+
+		else if (amount / (pennies) > 1) {
+			var numPennies = Math.floor((amount) / (pennies));
+			console.log("Pennies: " + numPennies);
+			console.log(amount)
+		}
+
+
+		// if (Math.floor(amount) >= 1) {
+		// 	console.log("Pennies: " +amount)
+		// 	else {
+		// 		console.log("No pennies");
+		// 	}
+		
+
+}
+
+
+// Write a program that will convert a dollar amount to the number of coins that make up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.
+
